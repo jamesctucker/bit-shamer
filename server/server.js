c8c6d71e674f19b4ac8475b21443b815ca29b049
James Tucker <jamesctucker@protonmail.com>
2019-03-16 14:19:38 -0500

    installed bitcoinaverage

---
+++ server/server.js
@@ -0,0 +1,40 @@
+const express = require('express');
+require('dotenv').config();
+
+
+const app = express();
+const bodyParser = require('body-parser');
+// const sessionMiddleware = require('./modules/session-middleware');
+
+
+// Route includes
+const apiRouter = require('./api.router');
+
+
+
+
+// Body parser middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Passport Session Configuration //
+// app.use(sessionMiddleware);
+
+
+
+/* Routes */
+app.use('/api', apiRouter);
+
+
+// Serve static files
+app.use(express.static('build'));
+
+
+// App Set //
+const PORT = process.env.PORT || 5000;
+
+/** Listen * */
+app.listen(PORT, () => {
+    console.log(`Listening on port: ${PORT}`);
+});
+
